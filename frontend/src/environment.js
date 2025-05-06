991ab42b0b6a7e739181859f1f90d6208cb5bbaa
Jaiswalsahil990 <jaiswalsahil283@gmail.com>
2025-05-06 18:01:45 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://videoapplication-o4b4.onrender.com" :
+    "https://sahil-jaiswal-production-backend.onrender.com" :
 
     "http://localhost:8000"
 
